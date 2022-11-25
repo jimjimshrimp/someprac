b339ba2e9ef0007c44da808f18f4f6382a897c5a
hahajh <2992895273@qq.com>
2022-11-25 11:37:45 +0800

    linkedlistV2

--- linkedlist.c
+++ linkedlist.c
@@ -8,11 +8,20 @@ typedef struct Node {
 myNode *addNode();
 myNode *addNodeBack();
 void display(myNode *head);
+void findVal(int val, myNode *head);
+void insertMiddle(int n, myNode * head, myNode * newNode);
+void removeMiddle(int n, myNode * head);
 
 int main() {
   myNode *HEAD = NULL;
   HEAD = addNodeBack();
-  display(HEAD);
+  //display(HEAD);
+  //findVal(10, HEAD);
+  myNode * newNode =(myNode *)malloc(sizeof(myNode));
+  newNode->val=4;
+  newNode->next=NULL;
+  //insertMiddle(9, HEAD, newNode);
+  removeMiddle(3,HEAD);
 }
 
 myNode *addNode() {
@@ -22,9 +31,9 @@ myNode *addNode() {
     temp = (myNode *)malloc(sizeof(myNode));
     temp->val = i;
     temp->next = NULL;
-    if (head == NULL) {  //if head is empty, then make temp the head.
+    if (head == NULL) {
       head = temp;
-    } else {  //otherwise, temp points to previous head and becomes the new head.
+    } else {
       temp->next = head;
       head = temp;
     }
@@ -36,29 +45,89 @@ myNode *addNodeBack() {
   myNode *temp = NULL;
   myNode *head = NULL;
   myNode *p = NULL;
-  for (int i = 1; i < 6; i++) {
+  for (int i = 1; i < 9; i++) {
     temp = (myNode *)malloc(sizeof(myNode));
     temp->val = i;
     temp->next = NULL;
-    if (head == NULL) {  // if head is empty, then make temp the head.
+    if (head == NULL) {
       head = temp;
-    } else if (head->next == NULL) { // else if head points to NULL, then add temp to head.
+    } else if (head->next == NULL) {
       head->next = temp;
-    } else {  // else if head points to another node, make a pointer p to traverse through the linkedlist
+    } else {
       p = head;
       while (p->next != NULL) {
         p = p->next;
       }
-      p->next = temp;  // find the node that points to null and add temp behind it.
+      p->next = temp;
     }
   }
   return head;
 }
 
 void display(myNode *head) {
-  myNode *p = head;  // make a new head pointer to traverse through the linkedlist that needs to be displayed.
+  myNode *p = head;
   while (p) {
     printf("%d", p->val);
     p = p->next;
   }
+}
+
+void findNth(int n, myNode *head) {
+  myNode *p = NULL;
+  p = head;
+  int i = 0;
+  while (i < n) {
+    p = p->next;
+  }
+}
+
+void findVal(int val, myNode *head) {
+  myNode *p = NULL;
+  p = head;
+  int i = 0;
+  int found = 0;
+  while (p != NULL){
+      if (val == p->val){
+        printf("\n已找到，id为：%d.",p->val);
+        found = 1;
+        }
+      p = p->next;
+      i++;
+    }
+    if (found == 0){
+      printf("\ncannot find the value");
+    }
+}
+
+void insertMiddle(int n, myNode * head, myNode * newNode){
+  myNode *p = NULL;
+  p = head;
+  int i = 0;
+  while (i!=n-1){
+    if(p->next == NULL){
+      printf("sorry,cannot insert");
+      return;
+    }
+    p = p->next;
+    i++;
+  }
+  newNode->next = p->next;
+  p->next = newNode;
+  display(head);
+}
+
+void removeMiddle(int n, myNode * head){
+  myNode *p = NULL;
+  p = head;
+  int i = 0;
+  while (i!=n-1){
+    if(p->next == NULL){
+      printf("sorry,cannot remove");
+      return;
+    }
+    p = p->next;
+    i++;
+  }
+  p->next = p->next->next;
+  display(head);
 }
